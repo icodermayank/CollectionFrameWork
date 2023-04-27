c02b6b9091a3109bc403e575c6d2ab30bd604b06
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-04-27 13:40:24 +0530

    HashSet Basic program

--- HashSetExample.java
+++ HashSetExample.java
@@ -1,15 +1,34 @@
 import java.util.HashSet;
+import java.util.*;
+
 public class HashSetExample {
    public static void main(String [] args){
+    // 
     HashSet <Object>  hs = new HashSet<>();
 
+   // HashSet<String> hset = new HashSet<>();
+
     hs.add(10);
+    hs.add(10);  // HashMap doesnot contains duplicate values.
     hs.add(23.45);
     hs.add('c');
     hs.add("Mayank");
     hs.add(0);
+    hs.add(null);
+    hs.add(null); // we can't add multiple null values.
 
     System.out.println(hs);
+
+
+
+    // printing One By One
+    Iterator itr = hs.iterator();
+
+    while(itr.hasNext()){
+        System.out.println(itr.next());
+    }
+
+  
 
    } 
 }
